6fcf018b2bb9e9917d039f68e79fb9824d8355b2
DilipDurge <46677831+DilipDurge@users.noreply.github.com>
2022-12-16 16:47:06 +0530

    Create SpeakingClockConstants.java

---
+++ src/main/java/com/wowcher/speakingclock/constants/SpeakingClockConstants.java
@@ -0,0 +1,12 @@
+package com.wowcher.speakingclock.constants;
+
+public class SpeakingClockConstants {
+    public static final String INVALID_TIME = "Invalid time format";
+    public static final String INVALID_HOUR = "Invalid hour value";
+    public static final String INVALID_MINUTE = "Invalid minute value";
+    public static final String AM = "am";
+    public static final String PM = "pm";
+    public static final String MID_DAY = "It's Midday";
+    public static final String MID_NIGHT = "It's Midnight";
+    public static final String ITS = "It's ";
+}
